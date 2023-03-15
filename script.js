961dc856813b3262938003f883fdf52be90796d7
matteovcc <matteovcc99@gmail.com>
2023-03-15 15:22:36 +0100

    Bottone rimuovi

--- script.js
+++ script.js
@@ -15,23 +15,30 @@ fetch("https://striveschool-api.herokuapp.com/books")
         <div class="card-body">
             <h5 class="card-title">${book.title}</h5>
             <p class="card-text">${book.price} Euro</p>
+            <button class="btn btn-danger px-2 py-1 w-100 fw-semibold">Rimuovi</button>
         </div>
         </div>
     `;
 
+    const removeBtn = col.querySelector(".btn")
+    removeBtn.onclick = () => {
+        col.remove()
+    }
+
       grid.appendChild(col);
 
-      const cardBody = document.getElementById("card-body")
-
-      const button = document.createElement("button");
-      button.className = "btn btn-danger";
-      button.innerText = "Scarta";
-      button.onclick = () => {
-          col.remove();
-      };
-
+      
       
       
     });
-  })
-  .catch((error) => console.log("errore", error));
+})
+.catch((error) => console.log("errore", error));
+
+//   const cardBody = document.getElementById("card-body")
+
+//   const button = document.createElement("button");
+//   button.className = "btn btn-danger";
+//   button.innerText = "Scarta";
+//   button.onclick = () => {
+//       col.remove();
+//   };
